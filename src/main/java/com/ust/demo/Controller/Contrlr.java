eb199fe9d6249c2efe85cadf0a0da5fb0e0cac71
Blesson Abraham <blesson.abraham1@ust.com>
2023-05-22 13:14:29 +0530

    updates

---
+++ src/main/java/com/ust/demo/Controller/Contrlr.java
@@ -0,0 +1,18 @@
+package com.ust.demo.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/demo")
+public class Contrlr {
+    @GetMapping("/welcome")
+    public String welcome(){
+        return "welcome Blesson";
+    }
+    @GetMapping("/blesson")
+    public String welcomeAadil(){
+        return "welcome Blesson J A";
+    }
+}
